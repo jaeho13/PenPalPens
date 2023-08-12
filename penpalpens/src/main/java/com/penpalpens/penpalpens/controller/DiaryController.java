7f0ffe72d59305fc5990f3a29a76bc824a7260ad
kimyuri <dsuis1310009@naver.com>
2023-08-13 00:25:34 +0900

    Get All Diary List

--- penpalpens/src/main/java/com/penpalpens/penpalpens/controller/DiaryController.java
+++ penpalpens/src/main/java/com/penpalpens/penpalpens/controller/DiaryController.java
@@ -1,18 +1,19 @@
 package com.penpalpens.penpalpens.controller;
 
+import com.penpalpens.penpalpens.entity.Diary;
 import com.penpalpens.penpalpens.entity.UserInfo;
 import com.penpalpens.penpalpens.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -31,4 +32,13 @@ public class DiaryController {
         diaryService.createDiary(diary, userInfo);
     }
 
+    // 글 리스트
+    @GetMapping("/diary")
+    public List<Diary> ListDiary(HttpServletRequest request) throws ParseException {
+        System.out.println("전체 글 리스트 불러오기 호출");
+        HttpSession session = request.getSession(false);
+        UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
+        List<Diary> list = new ArrayList<>();
+        return list = diaryService.ListDiary(userInfo);
+    }
 }
--- penpalpens/src/main/java/com/penpalpens/penpalpens/service/DiaryService.java
+++ penpalpens/src/main/java/com/penpalpens/penpalpens/service/DiaryService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -34,11 +36,15 @@ public class DiaryService {
         System.out.println("글 게시완료");
         diaryRepository.save(d);
 
-//            JBoard j = new JBoard();
-//            j.setBTitle((String) board.get("boardTitle"));
-//            j.setBContent((String) board.get("boardContent"));
-//            Member memberVO = memberService.findUser((String) board.get("memberAccount"));
-//            j.setMemberVO(memberVO);
-//            boardRepository.save(j);
+    }
+
+
+    public List<Diary> ListDiary(UserInfo userInfo) {
+        String uEmail = userInfo.getUEmail();
+        System.out.println("회원 조회 후 그 회원이 쓴 글 가져오기 : "+ uEmail);
+        List<Diary> list = diaryRepository.findMyDiary(uEmail);
+        System.out.println("전체 글 리스트");
+
+        return list;
     }
 }
