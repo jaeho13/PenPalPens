aa59ee42776426e7fd12a9d45259610b87af1f0a
kimyuri <dsuis1310009@naver.com>
2023-08-12 21:35:17 +0900

    test

--- penpalpens/src/main/java/com/penpalpens/penpalpens/controller/ShareController.java
+++ penpalpens/src/main/java/com/penpalpens/penpalpens/controller/ShareController.java
@@ -6,10 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,5 +53,4 @@ public class ShareController {
         Boolean bool = true;
         return bool;
     }
-
 }
