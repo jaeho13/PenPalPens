4fd42d9d6a808b8effd2436232492ebd0fd2f547
kimyuri <dsuis1310009@naver.com>
2023-08-13 00:50:15 +0900

    yuri

--- penpalpens/src/main/java/com/penpalpens/penpalpens/controller/DiaryController.java
+++ penpalpens/src/main/java/com/penpalpens/penpalpens/controller/DiaryController.java
@@ -7,12 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
